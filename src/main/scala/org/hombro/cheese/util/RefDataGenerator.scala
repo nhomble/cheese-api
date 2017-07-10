9ebdc88d2674ee9a0256e90130456ddcb81968dd
nicolas <nhomble@terpmail.umd.edu>
2017-07-09 21:05:16 -0400

    laziness to the max

--- src/main/scala/org/hombro/cheese/util/RefDataGenerator.scala
+++ src/main/scala/org/hombro/cheese/util/RefDataGenerator.scala
@@ -9,8 +9,17 @@ object RefDataGenerator extends App {
   val client = CheeseClient()
   val enricher = WikiClient()
 
-  val cheeses = client.getCheeseNames(startingWith = "s").par.toSet
+  val cheeses = client.getCheeseNames().par.toSet
   val info = cheeses flatMap (cheese => client.getCheeseInfo(cheese)) map enricher.enrichCheeseInfo
-  val filtered = info filter (info => info.wikiLink.isDefined && info.imgLink.isDefined)
-  val cheeseToInfo = filtered.groupBy[String](_.name)
+  val filtered = info filter (info => info.wikiLink.isDefined && info.cheeseImg.isDefined)
+  val cheeseToInfo = filtered.groupBy[String](_.name) mapValues (infos => infos.head)
+
+  // UGH
+  println("{")
+  for((cheese, info) <- cheeseToInfo.seq){
+    print("\"%s\":".format(cheese))
+    print(info.toJson)
+    println(",")
+  }
+  println("}")
 }
